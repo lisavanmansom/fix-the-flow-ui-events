32e21bab0023c1a627bacd1f486fb0b35624c80c
lisavanmansom <lisavanmansom@gmail.com>
2023-12-11 11:46:29 +0100

    feedback en feedforward
    css + java

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,42 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Button feedback
+let feedback = document.querySelector('a:nth-of-type(15)')
+let body = document.querySelector('body')
+
+feedback.addEventListener("mouseover", hovered, false);
+feedback.addEventListener("mouseout", hoveredOut, false);
+
+function hovered (e) {
+  console.log("Hovered!");
+  body.classList.add('background')
+
+}
+
+function hoveredOut(e) {
+  console.log("Hovered Away!");
+  body.classList.remove('background')
+}
+
+// Button feedforward
+let feedforward = document.querySelector('a:nth-of-type(16)')
+
+feedforward.addEventListener("mousedown", mousePressed, false);
+feedforward.addEventListener("mouseup", mouseReleased, false);
+feedforward.addEventListener("click", mouseClicked, false);
+
+function mousePressed(e) {
+  console.log("Mouse is down!");
+}
+
+function mouseReleased(e) {
+  console.log("Mouse is up!");
+}
+
+function mouseClicked(e) {
+  console.log("Mouse is clicked!");
+  feedforward.classList.add('buttonrotate')
+}
+
